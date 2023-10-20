0aa24161c809542ca3a65f75b9d2c542fc490b70
valDEVid <daval.locub@gmail.com>
2023-10-20 15:17:51 +0200

    v1.2

--- deftry.py
+++ deftry.py
@@ -10,9 +10,6 @@ from replit import clear
 
 # Global Variables
 
-usrfielddic = "./wordlists/usermatch.txt"
-passfielddic = "./wordlists/passwdmatch.txt"
-
 class fg:
     BLACK   = '\033[30m'
     RED     = '\033[31m'
@@ -26,6 +23,7 @@ class fg:
 
 oneliner = "                                         \r"
 
+# Tool
 
 def parse_flags():
     parser = argparse.ArgumentParser(description='Default Credentials Checker')
@@ -37,6 +35,7 @@ def parse_flags():
     return args
 
 
+# Manage connections
 def conn(url, method, payload=None):
     if method == "post":
         r = requests.post(url, data=payload)
@@ -45,6 +44,16 @@ def conn(url, method, payload=None):
     return r
 
 
+# Prevent relative path python issues
+def abspath(file):
+    path = __file__
+    path = path.split("\\")
+    del path[-1]
+    path = "/".join(path) + "/" + file
+    return path
+
+
+# Make the payloads to send
 def make_payload(user_field="username", pwd_field="password", username="IfThisLoginIKillMyself", password="IfThisLoginIKillMyself"):
     payload = {
          f"{user_field}": f"{username}",
@@ -53,6 +62,7 @@ def make_payload(user_field="username", pwd_field="password", username="IfThisLo
     return payload
 
 
+# Read dictionaries
 def read_dict(dic):
     dictlist = []
     with open(dic, "r") as dict:
@@ -62,10 +72,12 @@ def read_dict(dic):
     return dictlist
 
 
+# Field manager
 def match_field(field, dic):
     dict = read_dict(dic)
     for i in field:
         for n in dict:
+            print(i, n)
             if i == n:
                 matched_field = i
             else:
@@ -73,18 +85,61 @@ def match_field(field, dic):
     return matched_field
 
 
-def get_fields(args):
+def choice(field):
+    while True:
+        try:
+            for i in range(0, len(field)):
+                print(f"{fg.CYAN}[{i}]:{fg.RESET} {fg.YELLOW}{field[i]}{fg.RESET}")
+            selec = int(input(f"{fg.BLUE}Pick with numbers: {fg.RESET}"))
+            print("\n")
+            fselec = field[selec]
+            break
+        except ValueError:
+            print(f"You need to choice with numbers...")
+    return fselec
+
+
+def no_empty(field):
+    fields = []
+    for i in field:
+        n = re.findall(r".*?=\"(.*?)\"", i)
+        if len(n[0]) != 0:
+            fields.append(i)
+        else:
+            continue
+    return fields
+
+
+def get_fields(args, usr, pwd):
     p1 = log.progress(f'{fg.YELLOW}Searching login fields{fg.RESET}')
     url = args.url
     login = conn(url, "get")
-    usr_field = re.findall(r"<input type=\"text\" name=\"(.*?)\"", login.text)
-    pass_field = re.findall(r"<input type=\"password\" name=\"(.*?)\"", login.text)
-    usr_field = match_field(usr_field, usrfielddic)
-    pass_field = match_field(pass_field, passfielddic)
+    #usr_field = re.findall(r"<input type=\"text\" name=\"(.*?)\"", login.text)
+    usr_element = re.findall(r"<input type=\"text\".*?name=\".*?\".*?id=\".*?\".*?>", login.text)
+    #pass_field = re.findall(r"<input type=\"password\" name=\"(.*?)\"", login.text)
+    pass_element = re.findall(r"<input type=\"password\".*?name=\".*?\".*?id=\".*?\".*?>", login.text)
+    usr_field = re.findall(r" (.*?=\".*?\")", usr_element[0])
+    pass_field = re.findall(r" (.*?=\".*?\")", pass_element[0])
+    usr_field = no_empty(usr_field)
+    pass_field = no_empty(pass_field)
+    if len(usr_field) > 1:
+        print(f"{fg.BLUE}More than 1 element for username match!!{fg.RESET}\n")
+        usr_field = choice(usr_field)
+    if len(pass_field) > 1:
+        print(f"{fg.BLUE}More than 1 element for password match!!{fg.RESET}\n")
+        pass_field = choice(pass_field)
+    else:
+        if usr_field == None:
+            usr_field = match_field(usr_field, usr)
+        elif pass_field == None:
+            pass_field = match_field(pass_field, pwd)
     if usr_field == None or pass_field == None:
         print(f"\n\n\n{fg.RED}[!] Cant find any good login field, sorry...\n{fg.RESET}"); exit(1)
-    print(f'{fg.GREEN}Got the correct fields lucky bastard!: ' + f"\"{usr_field}\" & \"{pass_field}\"\n{fg.RESET}")
-    return (usr_field, pass_field)
+    p1.success
+    usr_field = re.findall(r".*?=\"(.*?)\"", usr_field)
+    pass_field = re.findall(r".*?=\"(.*?)\"", pass_field)
+    print(f'{fg.GREEN}Got the correct fields lucky bastard!: ' + f"\"{usr_field[0]}\" & \"{pass_field[0]}\"\n{fg.RESET}")
+    return (usr_field[0], pass_field[0])
 
    
 def get_error_values(response):
@@ -132,26 +187,35 @@ def brute_force(usr_field, pass_field, url, usrdic, passdic, errors=None):
             
 
 def main():
+    # Print Headers and read flags
     print(f"\n{fg.MAGENTA}[*] Welcome to deftry{fg.RESET}")
     print(f"{fg.MAGENTA}_{fg.RESET}" * 30 + "\n")
     args = parse_flags()
-    usrdic = "./wordlists/usr.txt"
-    passdic = "./wordlists/pass.txt"
+    
+    # Prepare dictionaries
+    usrdic = abspath("wordlists/usr.txt")
+    passdic = abspath("wordlists/pass.txt")
+    usrfielddic = abspath("wordlists/usermatch.txt")
+    passfielddic = abspath("wordlists/usermatch.txt")
     if args.inputuserdic:
         usrdic = args.inputuserdic
     elif args.inputpassdic:
         passdic = args.inputpassidc
     
-    usr_field, pass_field = get_fields(args)
-    if not args.nocheck:
+    # Get the correct fields
+    usr_field, pass_field = get_fields(args, usrfielddic, passfielddic)
+    if not args.nocheck:  # This is if user dont want to check the response for autodetect a correct input
         error_payload = make_payload(usr_field, pass_field)
         err_response = conn(args.url, "post", error_payload)
         errors = get_error_values(err_response)
     else:
         errors= None
+    
+    # Start the brute force
     brute_force(usr_field, pass_field, args.url, usrdic, passdic, errors)
 
 
+# Ctrl C controled exit
 def def_handler(sig, frame):
 	print(f"\n\n\n{fg.RED}[!] Saliendo...{fg.RESET}\n")
 	exit(1)
